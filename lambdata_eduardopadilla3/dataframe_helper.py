4bea5981c9a5bb7e17285de45952777e53faab77
eduardopadilla3 <eduardopadilla123@yahoo.com>
2020-08-05 14:36:19 -0500

    new function

--- lambdata_eduardopadilla3/dataframe_helper.py
+++ lambdata_eduardopadilla3/dataframe_helper.py
@@ -5,7 +5,7 @@ some function to help cleaning and handling dataframes
 
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+
 
 
 def display_settings(df):
@@ -17,10 +17,8 @@ def display_settings(df):
 
 def train_validate_test(df):
     s = input("Enter target col:")
-    X = df
-    y = X.pop(s)
-    X_train, X_test, y_train, y_test =
-    train_test_split(X, y, test_size=0.2, random_state=1)
-    X_train, X_val, y_train, y_val =
-    train_test_split(X_train, y_train, test_size=0.25, random_state=1)
+    X_train, X_val, X_test = np.split(df.sample(frac=1), [int(.6*len(df)), int(.8*len(df))])
+    y_train = X_train.pop(s)
+    y_val = X_val.pop(s)
+    y_test = X_test.pop(s)
     return (X_train, X_val, X_test, y_train, y_val, y_test)
